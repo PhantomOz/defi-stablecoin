8b1635b9cb930f7b5db4eb8ddc71c8a9d3193d04
PhantomOz <faniogor@gmail.com>
2024-02-26 04:11:36 +0100

    This will add ISCEngine Interface

--- src/SCEngine.sol
+++ src/SCEngine.sol
@@ -5,4 +5,17 @@ pragma solidity ^0.8.19;
 /// @author Favour Aniogor
 /// @notice This helps keeps Stable Coin at constant exchange rate.
 /// @dev This system is designed to help Stable Coin maintain a 1 token = $1 peg.
-contract SCEngine {}
+contract SCEngine {
+
+}
+
+interface ISCEngine {
+    function depositCollateralAndMintSC() external;
+    function depositCollateral() external;
+    function redeemCollateralForSC() external;
+    function redeemCollateral() external;
+    function mintSC() external;
+    function burnSC() external;
+    function liquidate() external;
+    function getHealthFactor() external view;
+}
