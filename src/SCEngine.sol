e87261557ac09a1149915d45bf5491f4c780f6c9
PhantomOz <faniogor@gmail.com>
2024-02-26 05:05:34 +0100

    This will fufil depositCollateral function7

--- src/SCEngine.sol
+++ src/SCEngine.sol
@@ -2,10 +2,14 @@
 pragma solidity ^0.8.19;
 
 import { StableCoin } from "./StableCoin.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 
 /// errors
 error SCEngine__MustBeMoreThanZero();
 error SCEngine__tokensAndPriceFeedsArrayMustBeSameLength();
+error SCEngine__TokenNotAllowed();
+error SCEngine__TransferFailed();
 
 /// @title Stable Coin Engine
 /// @author Favour Aniogor
@@ -16,12 +20,22 @@ contract SCEngine {
     /// State Variables
     /// @dev maps the approved colleteral tokens to their price feed
     mapping(address token => address priceFeed) private s_priceFeeds;
+    mapping(address user => mapping(address token => uint256 balance)) private s_addressToCollateralDeposited;
     StableCoin private immutable i_SC;
+
+    /// Events
+    event CollateralDeposited(address indexed _sender, address indexed _token, uint256 _value);
 
     /// Modifiers
     modifier moreThanZero(uint256 _amount){
         if(_amount <= 0) {
             revert SCEngine__MustBeMoreThanZero();
+        }
+        _;
+    }
+    modifier isTokenAllowed(address _token) {
+        if(s_priceFeeds[_token] == address(0)){
+            revert SCEngine__TokenNotAllowed();
         }
         _;
     }
@@ -39,7 +53,14 @@ contract SCEngine {
     
     /// @param _tokenCollateralAddress The address of the token to deposit as collateral
     /// @param _amountOfCollateral The amount of collateral to deposit
-    function depositCollateral(address _tokenCollateralAddress, uint256 _amountOfCollateral) external moreThanZero(_amountOfCollateral){}
+    function depositCollateral(address _tokenCollateralAddress, uint256 _amountOfCollateral) external moreThanZero(_amountOfCollateral) isTokenAllowed(_tokenCollateralAddress){
+        s_addressToCollateralDeposited[msg.sender][_tokenCollateralAddress] += _amountOfCollateral;
+        emit CollateralDeposited(msg.sender, _tokenCollateralAddress, _amountOfCollateral);
+        bool _success = IERC20(_tokenCollateralAddress).transferFrom(msg.sender, address(this), _amountOfCollateral);
+        if(!_success){
+            revert SCEngine__TransferFailed();
+        }
+    }
 }
 
 interface ISCEngine {
