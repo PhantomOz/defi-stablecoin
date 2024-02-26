e1a060c6a9b4e12f127e66124abebeb400295bbc
PhantomOz <faniogor@gmail.com>
2024-02-26 04:00:02 +0100

    This will create the SCEngine.sol

---
+++ src/SCEngine.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/// @title Stable Coin Engine
+/// @author Favour Aniogor
+/// @notice This helps keeps Stable Coin at constant exchange rate.
+/// @dev This system is designed to help Stable Coin maintain a 1 token = $1 peg.
+contract SCEngine {}
